46cec432f0144b25c8abd9c1acb3fc7451bcc19a
Medtex <medtexprog@gmail.com>
2024-10-29 16:22:40 +0800

    day8

---
+++ src/School/Location.java
@@ -0,0 +1,15 @@
+package School;
+
+import java.util.Scanner;
+
+class Location {
+    public int row;
+    public int column;
+    public double maxValue;
+
+    public Location(int row, int column, double maxValue) {
+        this.row = row;
+        this.column = column;
+        this.maxValue = maxValue;
+    }
+}
